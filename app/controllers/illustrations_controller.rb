c6921a71a3501306e8a56f15dfc3eeeccde586bb
wali <larts393@gmail.com>
2021-10-29 03:55:04 +0800

    整理程式碼

--- app/controllers/illustrations_controller.rb
+++ app/controllers/illustrations_controller.rb
@@ -1,4 +1,5 @@
 class IllustrationsController < ApplicationController
+    before_action :find_illustration, only: [:edit, :update, :destroy]
     def index
         @illustrations = Illustration.all
     end
@@ -18,31 +19,21 @@ class IllustrationsController < ApplicationController
     end
 
     def edit
-        @illustration = Illustration.find_by(id: params[:id])
     end
 
     def update
-        @illustration = Illustration.find_by(id: params[:id])
-
         if @illustration.update(illustration_params)
             redirect_to illustrations_path, notice: "作品資料更新成功！"
         else
             render :edit
         end
     end
-
-    def destroy
-        @illustration = Illustration.find_by(id: params[:id])
-        @illustration.destroy if @illustration
-        redirect_to illustrations_path, notice: "作品資料已刪除！"
-    end
-
+    
     def show
     end
 
 
     def destroy
-        @illustration = Illustration.find_by(id: params[:id])
         @illustration.destroy if @illustration
         redirect_to illustrations_path, notice: "作品資料已刪除！"
         # render html: "destroy"
@@ -51,6 +42,10 @@ class IllustrationsController < ApplicationController
     private
     def illustration_params
         params.require(:illustration).permit(:name, :description, :years, :label, :image)
+    end
+
+    def find_illustration
+        @illustration = Illustration.find_by(id: params[:id])
     end
 
 end
