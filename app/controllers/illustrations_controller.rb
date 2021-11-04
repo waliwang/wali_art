41aa127734dbd329dae7e8fcc17aef6509424dc3
wali <larts393@gmail.com>
2021-11-05 00:35:43 +0800

    修改view

--- app/controllers/illustrations_controller.rb
+++ app/controllers/illustrations_controller.rb
@@ -4,10 +4,43 @@ class IllustrationsController < ApplicationController
         @illustrations = Illustration.all
     end
 
+    def create
+        @illustration = Illustration.new(illustration_params)
+
+        if @illustration.save
+            #成功
+            redirect_to admin_illustrations_path, notice: "新增作品成功！"
+        else
+            #失敗
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @illustration.update(illustration_params)
+            redirect_to admin_illustrations_path, notice: "作品資料更新成功！"
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @illustration.destroy if @illustration
+        redirect_to admin_illustrations_path, notice: "作品資料已刪除！"
+        # render html: "destroy"
+    end
+
     
 
 
     private
+    def illustration_params
+        params.require(:illustration).permit(:title, :content, :years, :label, :image)
+    end
+
     def find_illustration
         @illustration = Illustration.find_by(id: params[:id])
     end
