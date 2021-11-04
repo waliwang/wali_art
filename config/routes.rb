733eb341188e36afa6cd3cd39b16e77494abbb2e
wali <larts393@gmail.com>
2021-11-04 21:50:39 +0800

    後台路徑增加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users 
 
+  namespace :admin do
+    resources :illustrations
+    resources :users
+  end
+
   resources :illustrations
   root  "welcome#index"
 
