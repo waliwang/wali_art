a683bb481f7e2bed2d452725a1dba866ee4dd423
wali <larts393@gmail.com>
2021-11-06 16:02:09 +0800

    寄信create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            ContactMailer.say_hello_to(@user).deliver_now
+            redirect_to @user, notice: 'User was successfully create.'
+        else
+            render :new
+        end
+    end
 end
