fb7c690dcd184ac285a6331123e81a7e57354e16
wali <larts393@gmail.com>
2021-11-07 15:14:10 +0800

    聯絡我們表單

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_29_165508) do
+ActiveRecord::Schema.define(version: 2021_11_07_071235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "contet"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "curriculumvitaes", force: :cascade do |t|
     t.string "name"
