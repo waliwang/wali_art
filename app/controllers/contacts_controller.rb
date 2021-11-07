fa3de02d6489fdb4c5b00ee1e47a095f353fd88a
wali <larts393@gmail.com>
2021-11-07 15:26:09 +0800

    聯絡我們controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,19 @@
 class ContactsController < ApplicationController
+    def new
+        @contact = Contact.new
+    end
+
+    def create
+        @contact = Contact.new
+        if @content.save
+            flash,note[:notice] = "Your message was successfully sent!"
+            ContactMailer.notify(@contact).deliver_letter
+        else
+            flash.now[:notice] = "Your message was fail to sent!"
+        end
+    end
+
+    def contact_params
+        params.require(:contact).permit(:name, :email, :content)
+    end
 end
